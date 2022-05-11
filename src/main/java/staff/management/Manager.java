668a88fb4c20447bb69856e022b212740d12b358
Jack Dickson <jackowendickson@gmail.com>
2022-05-11 14:26:32 +0100

    Created manager and writing some tests

--- src/main/java/staff/management/Manager.java
+++ src/main/java/staff/management/Manager.java
@@ -1,4 +1,17 @@
 package staff.management;
 
-public class Manager {
+import staff.Employee;
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, String NInumber, double salary, String deptName){
+        super(name, NInumber, salary);
+        this.deptName = deptName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
 }
--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Bob", "A2M2", 35000, "Books");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Bob", manager.getName());
+    }
+
+    @Test
+    public void hasNInumber(){
+        assertEquals("A2M2", manager.getNInumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(35000, manager.getSalary(), 0.0);
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("Books", manager.getDeptName());
+    }
+
 }
